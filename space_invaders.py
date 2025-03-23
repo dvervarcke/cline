a11a9d883f0ab4b6b40656c9565da0169e159f90
dvervarcke <75751299+dvervarcke@users.noreply.github.com>
2025-03-22 19:55:49 -0700

    dv4

--- space_invaders.py
+++ space_invaders.py
@@ -4,7 +4,7 @@ import sys
 import os
 from pygame.locals import *
 
-# Initialize pygame vs 3
+# Initialize pygame vs 4
 pygame.init()
 pygame.font.init()
 pygame.mixer.init()  # Initialize sound mixer
